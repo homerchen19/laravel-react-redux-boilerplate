97e384e2dcc5b98f4d2c924c3f760bd74279d178
xxhomey19 <xxhomey19@gmail.com>
2018-08-05 18:46:38 +1000

    Add styled-components

--- resources/assets/js/src/containers/about/index.js
+++ resources/assets/js/src/containers/about/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
 
 const About = () => (
-    <section>
-        <h1>About Page</h1>
-    </section>
+    <Wrapper>
+        <Title>About Page</Title>
+    </Wrapper>
 );
 
 export default About;
