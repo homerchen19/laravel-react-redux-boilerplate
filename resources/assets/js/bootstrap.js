28ab1ddd5ca26e20440604a95312fd5c3e17596f
xxhomey19 <xxhomey19@gmail.com>
2018-08-06 13:14:25 +1000

    Add X-CSRF-TOKEN in axios headers

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -23,8 +23,12 @@ try {
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute('content'),
+};
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
