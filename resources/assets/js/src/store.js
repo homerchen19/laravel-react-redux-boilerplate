02d59135e429a48818dd44e967ac5ec4bb19b09f
xxhomey19 <xxhomey19@gmail.com>
2018-08-14 11:02:53 +1000

    Add redux-logger

--- resources/assets/js/src/store.js
+++ resources/assets/js/src/store.js
@@ -1,11 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import rootReducer from './reducers';
 
-const initialState = {};
 const middleware = [thunk];
 
-const composedEnhancers = compose(applyMiddleware(...middleware));
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+}
 
-export default createStore(rootReducer, initialState, composedEnhancers);
+const configureStore = createStore(rootReducer, applyMiddleware(...middleware));
+
+export default configureStore;
